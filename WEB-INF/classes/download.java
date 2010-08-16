7c0a53e31a3aee3abfe29996d2bcf92768f9b1d5
kwolf <kwolf@8df75f15-59d7-5c4e-8359-c3bff8332439>
2010-08-16 11:38:46 +0000

    Fix warnings, formatting
    git-svn-id: http://pmo.applab.org:81/svn/ckw/products/pulse.server@289 8df75f15-59d7-5c4e-8359-c3bff8332439

--- WEB-INF/classes/download.java
+++ WEB-INF/classes/download.java
@@ -1,85 +1,74 @@
-
 import javax.servlet.http.*;
-
-import org.apache.commons.logging.Log;
 
 import PulseConfiguration.*;
 import java.io.*;
 
 public class download extends HttpServlet {
+    private static final long serialVersionUID = 1L;
 
-	
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
-	{
-		String requestString=request.getParameter("request");
-		String handsetId=request.getParameter("handset_id");
-		
-		try
-		{
-			sfConnect.login();
-			String phoneId = sfConnect.getPhoneId(handsetId);
-			if(!this.sendErrorMessage(phoneId, response))
-			{
-				this.sendResponse(requestString, phoneId, response);
-			}
-			sfConnect.logout();
-		}
-		catch(Exception e)
-		{
-			response.getWriter().write("The system has encountered an error");
-			StringWriter sw = new StringWriter();
-			e.printStackTrace(new PrintWriter(sw));
-			log(sw.toString());
-		}
-	}
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String requestString = request.getParameter("request");
+        String handsetId = request.getParameter("handset_id");
 
-	private void sendResponse(String request, String phoneId, HttpServletResponse response) throws Exception {
-		String content = "";
-		try
-		{
-			if(request.equals("messages")) {
-				content = sfConnect.getCKWMessages(phoneId);
-			}
-			else if (request.equals("profile")) {
-				content = sfConnect.getCKWProfile(phoneId);
-			}
-			else if (request.equals("performance")) {
-				content = sfConnect.getCKWPerformance(phoneId);
-			}
-			else {
-				content = "Invalid request.";
-			}
-			
-			if(content.equals(null) || content.equals("")) {
-				response.getWriter().write("You have no content for this tab.");
-			} else {
-				response.getWriter().write(content);
-			}
-		}
-		catch(NullPointerException e)
-		{
-			// This seems to mean that the object being queried doesn't exist in sales force yet
-			response.getWriter().write("There was a problem accessing this tab content. Please try again later or contact an administrator.");
-		}
-	}
+        try {
+            sfConnect.login();
+            String phoneId = sfConnect.getPhoneId(handsetId);
+            if (!this.sendErrorMessage(phoneId, response)) {
+                this.sendResponse(requestString, phoneId, response);
+            }
+            sfConnect.logout();
+        }
+        catch (Exception e) {
+            response.getWriter().write("The system has encountered an error");
+            StringWriter sw = new StringWriter();
+            e.printStackTrace(new PrintWriter(sw));
+            log(sw.toString());
+        }
+    }
 
-	private boolean sendErrorMessage(String phoneId,
-			HttpServletResponse response) throws Exception {
-		if(phoneId.equals("Duplicate IMEI"))
-		{
-			response.getWriter().write("Sorry, your phone is assigned to more than one person.");
-			return true;
-		}
-		else if(phoneId.equals("Not Registered"))
-		{
-			response.getWriter().write("Sorry, your phone is not registered.");
-			return true;
-		}
-		else if(phoneId.equals("System Error"))
-		{
-			response.getWriter().write("Sorry, an unknown system error has occured.");
-			return true;
-		}
-		return false;
-	}
+    private void sendResponse(String request, String phoneId, HttpServletResponse response) throws Exception {
+        String content = "";
+        try {
+            if (request.equals("messages")) {
+                content = sfConnect.getCKWMessages(phoneId);
+            }
+            else if (request.equals("profile")) {
+                content = sfConnect.getCKWProfile(phoneId);
+            }
+            else if (request.equals("performance")) {
+                content = sfConnect.getCKWPerformance(phoneId);
+            }
+            else {
+                content = "Invalid request.";
+            }
+
+            if (content.equals(null) || content.equals("")) {
+                response.getWriter().write("You have no content for this tab.");
+            }
+            else {
+                response.getWriter().write(content);
+            }
+        }
+        catch (NullPointerException e) {
+            // This seems to mean that the object being queried doesn't exist in sales force yet
+            response.getWriter().write(
+                    "There was a problem accessing this tab content. Please try again later or contact an administrator.");
+        }
+    }
+
+    private boolean sendErrorMessage(String phoneId, HttpServletResponse response) throws Exception {
+        if (phoneId.equals("Duplicate IMEI")) {
+            response.getWriter().write("Sorry, your phone is assigned to more than one person.");
+            return true;
+        }
+        else if (phoneId.equals("Not Registered")) {
+            response.getWriter().write("Sorry, your phone is not registered.");
+            return true;
+        }
+        else if (phoneId.equals("System Error")) {
+            response.getWriter().write("Sorry, an unknown system error has occured.");
+            return true;
+        }
+        return false;
+    }
 }
--- WEB-INF/classes/pulse.java
+++ WEB-INF/classes/pulse.java
@@ -2,43 +2,38 @@ import javax.servlet.http.*;
 import PulseConfiguration.*;
 
 public class pulse extends HttpServlet {
+    private static final long serialVersionUID = 1L;
 
-	
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-	{
-		String handset_id=request.getParameter("handset_id");
-		String message=request.getParameter("message");
-		//login the request
-		try
-		{
-			sfConnect.login();
-			//handset id's are tied to ckws. get the ckw id
-			String phone_id=sfConnect.getPhoneId(handset_id);
-			if(phone_id.equals("Duplicate IMEI"))
-			{
-				response.getWriter().write("The system has encountered an internal error. A duplicate IMEI has been detected");					
-			}
-			else if(phone_id.equals("Not Registered"))
-			{
-				response.getWriter().write("Sorry you cannot send a request because your phone is not registered");
-			}
-			else if(phone_id.equals("System Error"))
-			{
-				response.getWriter().write("Sorry you cannot send a request because an unexpected internal error has occured.");
-			}
-			else
-			{
-			//get the ckw id
-				String sfckw_id=sfConnect.getSalesforceCKW_ID(phone_id);
-				String ckw_id=sfConnect.getCKW_ID(phone_id);
-				String caseNumber=sfConnect.saveRequest(message,sfckw_id,ckw_id);
-				response.getWriter().write("Your request was received. One of our support specialists will get back to you shortly. Your support number is "+caseNumber);				
-			}
-			sfConnect.logout();
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+        String handset_id = request.getParameter("handset_id");
+        String message = request.getParameter("message");
+        // login the request
+        try {
+            sfConnect.login();
+            // handset id's are tied to ckws. get the ckw id
+            String phone_id = sfConnect.getPhoneId(handset_id);
+            if (phone_id.equals("Duplicate IMEI")) {
+                response.getWriter().write("The system has encountered an internal error. A duplicate IMEI has been detected");
+            }
+            else if (phone_id.equals("Not Registered")) {
+                response.getWriter().write("Sorry you cannot send a request because your phone is not registered");
+            }
+            else if (phone_id.equals("System Error")) {
+                response.getWriter().write("Sorry you cannot send a request because an unexpected internal error has occured.");
+            }
+            else {
+                // get the ckw id
+                String sfckw_id = sfConnect.getSalesforceCKW_ID(phone_id);
+                String ckw_id = sfConnect.getCKW_ID(phone_id);
+                String caseNumber = sfConnect.saveRequest(message, sfckw_id, ckw_id);
+                response.getWriter().write(
+                        "Your request was received. One of our support specialists will get back to you shortly. Your support number is "
+                                + caseNumber);
+            }
+            sfConnect.logout();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
